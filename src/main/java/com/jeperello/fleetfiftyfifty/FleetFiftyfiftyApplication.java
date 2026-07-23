1b38a6d8a609d7a11fe95ebf978abd7a61873065
jeperello <jorgeperello2020@gmail.com>
2026-07-23 20:39:32 -0300

    chore: initialize fleet-fiftyfifty spring boot project with java 21

---
+++ src/main/java/com/jeperello/fleetfiftyfifty/FleetFiftyfiftyApplication.java
@@ -0,0 +1,13 @@
+package com.jeperello.fleetfiftyfifty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetFiftyfiftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetFiftyfiftyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeperello/fleetfiftyfifty/FleetFiftyfiftyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeperello.fleetfiftyfifty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetFiftyfiftyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
